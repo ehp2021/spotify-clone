989f0bdaef6ab317cba2d5fd802ca6b126080624
ehp2021 <emmahpaik@gmail.com>
2021-12-14 12:11:06 -0600

    playlists

--- myapp/components/sidebar.tsx
+++ myapp/components/sidebar.tsx
@@ -8,92 +8,96 @@ import {
   Divider,
   Center,
   LinkBox,
-  LinkOverlay
+  LinkOverlay,
 } from '@chakra-ui/layout'
 import {
   MdHome,
-  MdLibraryMusic,
   MdSearch,
+  MdLibraryMusic,
   MdPlaylistAdd,
-  MdFavorite
+  MdFavorite,
 } from 'react-icons/md'
+import { usePlaylist } from '../lib/hooks'
 
-
-const navMenu =[
+const navMenu = [
   {
     name: 'Home',
     icon: MdHome,
-    route: '/'
+    route: '/',
   },
   {
     name: 'Search',
     icon: MdSearch,
-    route: '/search'
+    route: '/search',
   },
   {
     name: 'Your Library',
     icon: MdLibraryMusic,
-    route: '/library'
-  }
+    route: '/library',
+  },
 ]
 
 const musicMenu = [
   {
-    name: 'Create Playlists',
+    name: 'Create Playlist',
     icon: MdPlaylistAdd,
-    route: '/'
+    route: '/',
   },
   {
     name: 'Favorites',
     icon: MdFavorite,
-    route: '/favorites'
+    route: '/favorites',
   },
 ]
 
-const playlists = new Array(30).fill(1).map((_, i)=>`Playlist ${i+1}`)
+// const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
 
 const Sidebar = () => {
+  const { playlists } = usePlaylist()
   return (
-    <Box 
-      width="100%" 
-      height="calc(100vh - 100px)" 
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
       bg="black"
       paddingX="5px"
       color="gray"
-    > 
-      {/* content box, need to set 100% height to make it fill up its container */}
-      <Box paddingY="15px" height="100%"> 
-        {/* image box */}
-        <Box width="120px" marginLeft="20px" marginBottom="20px" paddingY="20px">
-          <NextImage src='/trax.svg' height={40} width={100}/>
+    >
+      <Box paddingY="20px" height="100%">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
-
         <Box marginBottom="20px">
           <List spacing={2}>
-            {navMenu.map(menu => (
+            {navMenu.map((menu) => (
               <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
                 <LinkBox>
                   <NextLink href={menu.route} passHref>
                     <LinkOverlay>
-                      <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
                       {menu.name}
                     </LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
-            )
-            )}
+            ))}
           </List>
         </Box>
-        
         <Box marginTop="20px">
           <List spacing={2}>
-            {musicMenu.map(menu => (
+            {musicMenu.map((menu) => (
               <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
                 <LinkBox>
                   <NextLink href={menu.route} passHref>
                     <LinkOverlay>
-                      <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
                       {menu.name}
                     </LinkOverlay>
                   </NextLink>
@@ -102,23 +106,19 @@ const Sidebar = () => {
             ))}
           </List>
         </Box>
-
-        <Divider color="gray.800"/>
+        <Divider color="gray.800" />
         <Box height="66%" overflowY="auto" paddingY="20px">
-          <List spacing={2}>
-            {playlists.map(playlist => (
-              <ListItem paddingX="20px" key={playlist}>
+          <List spaceing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
-                  <NextLink href="/">
-                    <LinkOverlay>
-                      {playlist}
-                    </LinkOverlay>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
             ))}
           </List>
-
         </Box>
       </Box>
     </Box>
---
+++ myapp/lib/hooks.ts
@@ -0,0 +1,33 @@
+import fetcher from './fetcher'
+import useSWR from 'swr'
+import { Playlist } from '@prisma/client'
+
+// need some hooks for pulling from api
+// you don't have tomake your own hooks but it makes things eaiser
+
+export const useMe = () => {
+  const { data, error} = useSWR('/me', fetcher)
+  // see what data is coming back
+  console.log(data) 
+
+  return {
+    user: data,
+    isLoading: !data && !error,
+    isError: error,
+  }
+}
+
+//for playlist it will be similar, we havent made an api route
+// make the hook for it anyways 
+// use it in teh sidebar, in the libraries
+// with swr, we can use this hook in multiple places and itwill only make ONE call 
+
+export const usePlaylist = () => {
+  const { data, error} = useSWR('/playlist', fetcher)
+
+  return {
+    playlists: (data as any) || [],
+    isLoading: !data && !error,
+    isError: error,
+  }
+}
